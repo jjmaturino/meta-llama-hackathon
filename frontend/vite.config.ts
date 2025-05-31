9642599c83da6d8ce3a65738e3c11b6b695ba975
Juan Maturino <juanjmaturino@gmail.com>
2025-05-31 12:35:28 -0400

    init: initialized react fe app using tanstack router and query

---
+++ frontend/vite.config.ts
@@ -0,0 +1,21 @@
+import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+
+import tailwindcss from "@tailwindcss/vite";
+
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { resolve } from "node:path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact(), tailwindcss()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  }
+});
