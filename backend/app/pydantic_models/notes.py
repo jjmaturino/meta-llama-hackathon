250dabca13e63e725a5c5bdba8cefacc5f7e1493
FRED <f22fan@uwaterloo.ca>
2025-05-31 15:01:01 -0400

    schema for notes

--- backend/app/pydantic_models/notes.py
+++ backend/app/pydantic_models/notes.py
@@ -1,1 +1,13 @@
 # do something here bruh
+
+from pydantic import BaseModel
+from typing import List
+
+class Note(BaseModel):
+    title: str
+    tags: List[str]
+    metadata: List[str]
+    questions: str
+    notes: str
+    summary: str
+    docs: List[str]
