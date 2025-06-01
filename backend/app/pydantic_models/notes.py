8b09a77524b69edb1ca8a22773527a720d377686
FRED <f22fan@uwaterloo.ca>
2025-06-01 01:02:29 -0400

    get all notes, get note by id working

--- backend/app/pydantic_models/notes.py
+++ backend/app/pydantic_models/notes.py
@@ -1,6 +1,8 @@
 # do something here bruh
 from pydantic import BaseModel
-from typing import List
+from typing import List, Union
+from .question import Question
+from .multiselect_question import MultiSelectQuestion
 
 class Note(BaseModel):
     id: int
@@ -10,3 +12,4 @@ class Note(BaseModel):
     notes: str
     summary: str
     docs: List[str]
+    questions: List[Union[Question, MultiSelectQuestion]]
