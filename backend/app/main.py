b4fb1286b179f0f089f38db346402454e4670ac9
FRED <f22fan@uwaterloo.ca>
2025-06-01 00:34:29 -0400

    added cors middleware to main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.router.instance import api_router
 
 app = FastAPI()
+
+# Add CORS middleware to accept all connections
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
@@ -10,6 +20,14 @@ def read_root():
 def get_application() -> FastAPI:
     application = FastAPI(
         title="llamas go yeerrrrr", openapi_url=f"/v1/openapi.json"
+    )
+
+    application.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
     )
 
     application.include_router(
